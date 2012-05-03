c1687732277f67545cfb390f8a58d17fc18886d7
Kevin Ngo <ktngo09@gmail.com>
2012-05-03 10:21:45 -0700

    wrap everything inside of object

--- draw_nothing.js
+++ draw_nothing.js
@@ -1,45 +1,54 @@
 $(document).ready(function (){
 
-    function init() {
+    function draw_nothing() {
+        var canvas;
+        var ctx;
+        var tool;
 
-        var canvas = $('#canvas');
-        var canvasElement = canvas.get(0);
+        this.init = function() {
+            canvas = $('#canvas');
+            var canvasElement = canvas.get(0);
 
-        // fit canvas to window
-        var ctx = canvasElement.getContext('2d');
-        ctx.canvas.width  = window.innerWidth;
-        ctx.canvas.height = window.innerHeight;
+            // fit canvas to window
+            ctx = canvasElement.getContext('2d');
+            ctx.canvas.width  = window.innerWidth;
+            ctx.canvas.height = window.innerHeight;
 
-        tool = new tool_pencil();
+            tool = new tool_pencil_point();
+            swap_tool(tool);
+        };
 
-        canvas.mousedown({'ctx': ctx}, tool.mousedown);
-        canvas.mouseup({'ctx': ctx}, tool.mouseup);
-        canvas.mousemove({'ctx': ctx}, tool.mousemove);
+        function swap_tool(tool) {
+            canvas.mousedown(tool.mousedown);
+            canvas.mouseup(tool.mouseup);
+            canvas.mousemove(tool.mousemove);
+        };
+
+        function tool_pencil_point() {
+            var tool = this;
+            this.started = false;
+
+            this.mousedown = function(e) {
+                tool.started = true;
+                ctx.fillStyle = 'rgb(200,0,0)';
+                ctx.fillRect (e.pageX - this.offsetLeft, e.pageY - this.offsetTop, 5, 5);
+            };
+
+            this.mouseup = function(e) {
+                tool.started = false
+            };
+
+            this.mousemove = function(e) {
+                if(tool.started) {
+                    ctx.fillStyle = 'rgb(200,0,0)';
+                    ctx.fillRect (e.pageX - this.offsetLeft, e.pageY - this.offsetTop, 5, 5);
+                }
+            };
+        }
+
     }
 
-    function tool_pencil() {
-        var tool = this;
-        this.started = false;
-
-        this.mousedown = function(e) {
-            tool.started = true;
-            e.data.ctx.fillStyle = 'rgb(200,0,0)';
-            e.data.ctx.fillRect (e.pageX - this.offsetLeft, e.pageY - this.offsetTop, 5, 5);
-        };
-
-        this.mouseup = function(e) {
-            tool.started = false
-        };
-
-        this.mousemove = function(e) {
-            if(tool.started) {
-                e.data.ctx.fillStyle = 'rgb(200,0,0)';
-                e.data.ctx.fillRect (e.pageX - this.offsetLeft, e.pageY - this.offsetTop, 5, 5);
-            }
-        };
-
-    }
-
-    init();
+    draw_nothing = new draw_nothing();
+    draw_nothing.init();
 
 });
