f7db005c70db6891d0660b220df58eefc7fad295
Kevin Ngo <ktngo09@gmail.com>
2012-06-30 05:26:28 -0700

    fix bug in button menu

--- js/draw_nothing.js
+++ js/draw_nothing.js
@@ -207,12 +207,13 @@ $(document).ready(function (){
                 if (button.selector == lastClickedButton) {
                     menu.css('visibility', 'hidden');
                     menuItems.css('visibility', 'hidden');
+                    lastClickedButton = '';
                 } else {
                     menu.css('visibility', 'visible');
                     menuItems.css('visibility', 'hidden');
                     pickerMenu.css('visibility', 'visible');
+                    lastClickedButton = button.selector;
                 }
-                lastClickedButton = button.selector;
             });
         }
 
